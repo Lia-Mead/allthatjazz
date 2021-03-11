b8277f80784d89b0fdddb6e44eb5b9e3ea204fab
Liat Meadows <liatmeadows@gmail.com>
2021-03-11 14:32:44 +0100

    css

--- client/src/newVenues.js
+++ client/src/newVenues.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { newThreeVens } from "./actions";
-// import axios from "./Axios";
 
 export default function NewVenues(props) {
     const dispatch = useDispatch();
