77d870a38c815108d9bbf934c0581a72dff71ca2
Liat Meadows <liatmeadows@gmail.com>
2023-06-26 17:14:51 +0300

    all that jazz - Refactor: remove tweets

--- client/src/feed.js
+++ client/src/feed.js
@@ -1,11 +1,11 @@
-import Tweets from "./tweets";
+// import Tweets from "./tweets";
 
 export default function Feed() {
     return (
         <>
             <div style={{ color: "white" }} className="feed bg-img">
                 <h1>All that Jazz News</h1>
-                <Tweets />
+                {/* <Tweets /> */}
             </div>
         </>
     );
